07320614901fca8d5788f537848aef8bcaf5c450
Lamboling Seans <hansfrank97@yahoo.com>
2014-06-09 03:02:37 +0700

    Added EntityBase

---
+++ src/io/github/lambo993/engine/EntityBase.java
@@ -0,0 +1,80 @@
+package io.github.lambo993.engine;
+
+/**
+ * A base for entity implemention
+ * @author Lambo993
+ * @since version 1.1.0
+ */
+public class EntityBase {
+
+	private int x, y, xVelocity , yVelocity;
+	private boolean isPaused = false;
+
+	/**
+	 * Gets the <code>Entity</code> X Location of the screen
+	 * @return the X Location of the screen
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * Gets the <code>Entity</code> X Velocity
+	 * @return The X velocity speed
+	 */
+	public int getXVelocity() {
+		return xVelocity;
+	}
+
+	/**
+	 * Gets the <code>Entity</code> Y Location of the screen
+	 * @return the Y Location of the screen
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Gets the <code>Entity</code> Y Velocity
+	 * @return The Y velocity speed
+	 */
+	public int getYVelocity() {
+		return yVelocity;
+	}
+
+	/**
+	 * Sets the <code>Entity</code> X coordinate of the screen
+	 * @param x the new X Location of the screen
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setXVelocity(int xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	/**
+	 * Sets the <code>Entity</code> Y coordinate of the screen
+	 * @param y the new Y Location of the screen
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setYVelocity(int yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+
+	public boolean isMoving() {
+		return getXVelocity() != 0 || getYVelocity() != 0;
+	}
+
+	public boolean isPaused() {
+		return isPaused;
+	}
+
+	public void setPaused(boolean paused) {
+		isPaused = paused;
+	}
+}
